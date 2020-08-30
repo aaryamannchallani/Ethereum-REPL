818ca6a22aab102b871fc89f516929d8e6dfd429
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-08-30 16:54:06 +0530

    added solc

---
+++ README.md
@@ -0,0 +1,20 @@
+# Custom REPL for Sandboxing on Ethereum
+---
+## Usage:
+-> `node index.js`
+---
+## Features:
+-> Top-Level await - no more callback hell 🎊
+-> `getInfuraProvider()` : returns Infura Provider connected to INFURA_KEY in env
+-> `getLocalProvider()` : returns provider connected to http://localhost:854 🎆
+-> `getWebSocketProvider(<optional>)` : defaults to wss://localhost:8546// 🕸
+-> `ethUtils` : Utility library derived from ethers.js 👷
+-> `getBalance(address)` : Mainnet Balance of given address 💰
+-> `availableUtils()` : returns available utilities from ethers.utils 🎡
+-> More Coming Soon...✨
+---
+## Roadmap:
+-> Adding DeFi contracts for easy interaction
+-> Easy interface to listen for contract events
+-> Artifact Maker
+---
---
+++ index.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+const repl = require("repl");
+const chalk = require("chalk");
+const ethers = require("ethers");
+const { utils: ethUtils } = require("ethers");
+const { table } = require("table");
+const solc = require("solc");
+const fs = require("fs");
+
+const functions = {
+  getInfuraProvider(network) {
+    return new ethers.providers.InfuraProvider(network, process.env.INFURA_KEY);
+  },
+  getLocalProvider() {
+    return new ethers.providers.JsonRpcProvider();
+  },
+  getWebSocketProvider() {
+    return new ethers.providers.WebSocketProvider();
+  },
+  getWebSocketProvider(host) {
+    return new ethers.providers.WebSocketProvider(host);
+  },
+  ethUtils,
+  ethers,
+  async getBalance(address) {
+    const p = new ethers.providers.InfuraProvider(
+      "homestead",
+      process.env.INFURA_KEY
+    );
+    return ethUtils.formatEther(await p.getBalance(address)) + " Ethers";
+  },
+  availableUtils() {
+    const u = Object.keys(ethUtils);
+    console.log(
+      table(
+        u.reduce(
+          (rows, key, index) =>
+            (index % 3 == 0
+              ? rows.push([key])
+              : rows[rows.length - 1].push(key)) && rows,
+          []
+        )
+      )
+    );
+  },
+  compileSolidity(path) {
+
+    const splitPath = path.split('/')
+    const fileName = splitPath[splitPath.length-1].split('.')[0]
+    const input = {
+      language: "Solidity",
+      sources: {
+        [fileName] : {
+          content: fs.readFileSync(path).toString(),
+        },
+      },
+      settings: {
+        outputSelection: {
+          "*": {
+            "*": ["*"],
+          },
+        },
+      },
+    };
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+    if(!fs.existsSync('./artifacts')){
+              fs.mkdirSync('./artifacts')
+        }
+          fs.writeFileSync(`./artifacts/${Object.keys(output.contracts)}.json`,JSON.stringify(output))
+  	return `Created Artifact at ./artifacts/${Object.keys(output.contracts)}.json`
+  },
+};
+
+const customRepl = repl.start(chalk.rgb(0, 255, 255)("Ethereum REPL>"));
+
+const defaultEval = customRepl.eval;
+customRepl.eval = (cmd, context, filename, callback) => {
+  defaultEval(cmd, context, filename, async (err, result) => {
+    if (err) {
+      // propagate errors from the eval
+      callback(err);
+    } else {
+      // awaits the promise and either return result or error
+      try {
+        callback(null, await Promise.resolve(result));
+      } catch (err) {
+        callback(err);
+      }
+    }
+  });
+};
+
+Object.assign(customRepl.context, functions);
